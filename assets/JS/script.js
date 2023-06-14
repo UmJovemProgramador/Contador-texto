8638378fafbd7cbe15228c0f0e5c38bc4074c34e
Antônio Lucas C Araújo <112925082+UmJovemProgramador@users.noreply.github.com>
2023-06-14 10:56:30 -0300

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,19 @@
-const texto = document.getElementById('input');
+const textoInserido = document.getElementById('input');
 const contarPalavras = document.getElementById('contarPalavras');
 const contarCaracteres = document.getElementById('contarCaracteres');
+const contarLinhas = document.getElementById('contarLinhas');
 
-texto.addEventListener('input', function()  {
-    const text = texto.value;
+textoInserido.addEventListener('input', function() {
+    const texto = textoInserido.value;
 
-    //Contar palavras
-    const palavras = text.trim().split(/\s+/);
-    contarPalavras.innerText = palavras.length;
+    // Contar palavras
+    const palavras = texto.trim().split(/\s+/);
+    contarPalavras.innerText = texto ? palavras.length : 0;
 
-    //Contar caracteres
-    
-    contarCaracteres.innerText = text.length;
+    // Contar caracteres
+    contarCaracteres.innerText = texto ? texto.length : 0;
 
-})
+    // Contar linhas
+    const linhas = texto.split('\n');
+    contarLinhas.innerText = texto ? linhas.length : 0;
+});
